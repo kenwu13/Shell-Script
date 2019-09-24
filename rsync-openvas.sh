63b9902c82c42a76e64add4ddb8f18cf01307d91
Ken Wu <ken594088@outlook.com>
2019-09-24 17:28:18 +0800

    Add files via upload

---
+++ rsync-openvas.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+LogDate=$(date +%F)
+Date1=$(date +%Y-%m%d-%R)
+
+User1=ezadmin
+Name1=rsync-openvas.sh
+
+cmd[1]=greenbone-nvt-sync
+cmd[2]=greenbone-certdata-sync
+cmd[3]=greenbone-scapdata-sync
+
+LogDir=/root/Log
+
+for cnt in $(seq 1 3)
+do
+	${cmd[${cnt}]}
+	if [ $? -eq 0 ]; then
+		echo "${cmd[${cnt}]} Succeeded."
+	else
+		echo "${cmd[${cnt}]} Failed."
+	fi
+done
+
+systemctl restart openvas-manager
+systemctl restart openvas-scanner

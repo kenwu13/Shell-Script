ac789c5b65de24a9c7f0805f0b3fe0bdf69bad21
kenwu13 <ken594088@gmail.com>
2020-02-07 17:44:56 +0800

    add Search

--- For-Nginx-Conf/nginx-conf.sh
+++ For-Nginx-Conf/nginx-conf.sh
@@ -2,7 +2,7 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-# 2020-02-04
+# 2020-02-07
 # Srv-List.txt = Host IP List
 # Usage: nginx-conf [<command>]
 # Available commands:
@@ -10,15 +10,17 @@ export PATH
 # Deploy  Push new configuration change
 # Restore Restore previous version
 # Clear   Delete backup's file
+# Search  Search Nginx config's file
 
-user=KenWu
 name=nginx-conf.sh
 
-User=KenWu
+User=ezadmin
 SrvList=./Srv-List.txt
 
+ezpass="@@u4u.6j;3"
+
 RestartNginxService() {
-rel=$(sudo /bin/rpm -q --qf "%{version}" -f /etc/redhat-release | /bin/cut -d. -f1)
+rel=$(/bin/rpm -q --qf "%{version}" -f /etc/redhat-release | /bin/cut -d. -f1)
 
 if [ "${rel}" = "7" ];then
     sudo /bin/systemctl restart nginx
@@ -40,10 +42,9 @@ DestDir[1]=/opt/APP/nginx/config/vhosts
 
 for cnt in $(seq 0 1)
 do
-	
 	if [ -d "${DestDir[${cnt}]}" ]; 
 	then
-		Array=($(sudo ls -l ${DestDir[${cnt}]} | /bin/awk {'print $9'} | /bin/grep '.conf$'))
+		Array=($(ls -l ${DestDir[${cnt}]} | /bin/awk {'print $9'} | /bin/grep '.conf$'))
 
 		for ((index=0; index<${#Array[@]}; index++));
 		do
@@ -68,10 +69,9 @@ DestDir[1]=/opt/APP/nginx/config/vhosts
 
 for cnt in $(seq 0 1)
 do
-	
 	if [ -d "${DestDir[${cnt}]}" ]; 
 	then
-		Array=($(sudo /bin/ls -l ${DestDir[${cnt}]} | /bin/awk {'print $9'} | /bin/grep '.conf$'))
+		Array=($(/bin/ls -l ${DestDir[${cnt}]} | /bin/awk {'print $9'} | /bin/grep '.conf$'))
 
 		for ((index=0; index<${#Array[@]}; index++));
 		do
@@ -94,10 +94,9 @@ DestDir[1]=/opt/APP/nginx/config/vhosts
 
 for cnt in $(seq 0 1)
 do
-	
 	if [ -d "${DestDir[${cnt}]}" ]; 
 	then
-		Array=($(sudo ls -l ${DestDir[${cnt}]} | /bin/awk {'print $9'} | /bin/grep '.conf$'))
+		Array=($(ls -l ${DestDir[${cnt}]} | /bin/awk {'print $9'} | /bin/grep '.conf$'))
 
 		for ((index=0; index<${#Array[@]}; index++));
 		do
@@ -109,6 +108,38 @@ do
 	else
 		echo "${DestDir[${cnt}]} does not exists."
 	fi
+done
+}
+
+SearchNginx() {
+SrcDir[0]=/tmp
+SrcDir[1]=/tmp
+
+DestDir[0]=/etc/nginx/conf.d
+DestDir[1]=/opt/APP/nginx/config/vhosts
+
+for cnt in $(seq 0 1)
+do
+	rel=$(/bin/rpm -q --qf "%{version}" -f /etc/redhat-release | /bin/cut -d. -f1)
+
+	echo "Hostname:${HOSTNAME}"
+	echo "OS:CentOS ${rel}"
+	echo "IP:$(ip addr | grep -w inet | grep -v 127.0.0.1 | cut -d/ -f1 | awk '{print $2}' | grep 10.10)"
+	echo ""
+
+	if [ -d "${DestDir[${cnt}]}" ]; 
+	then
+		Array=($(ls -l ${DestDir[${cnt}]} | /bin/awk {'print $9'} | /bin/grep '.conf$'))
+
+		for ((index=0; index<${#Array[@]}; index++));
+		do
+			ls "${DestDir[${cnt}]}"/"${Array[${index}]}"
+		done
+
+	else
+		echo "${DestDir[${cnt}]} does not exists."
+	fi
+	echo ""
 done
 }
 
@@ -156,13 +187,27 @@ case ${1} in
 	done
 ;;
 
+"Search")
+	while read line;
+	do
+		SrvS[${index}]="${line}"
+		index=$(expr ${index} + 1)
+	done < ${SrvList}
+	echo "[${index}]: ${SrvS[${index}]}"
+	for ((index=0; index<${#SrvS[@]}; index++));
+	do
+		typeset -f SearchNginx | sshpass -p "${ezpass}" ssh -T ${User}@${SrvS[$index]} "$(cat); SearchNginx"
+	done
+;;
+
 *)
 	echo "Usage: nginx-conf [<command>] 
 	Available commands: 
 	Restart Restart Nginx's service 
 	Deploy  Push new configuration change
 	Restore Restore previous version
-	Clear   Delete backup's file"
+	Clear   Delete backup's file
+	Search Search Nginx config's file"
 	;;
 
 esac
